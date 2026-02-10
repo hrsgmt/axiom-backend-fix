49917b9a7674bd00455e1c17c3ad77fce8659316
hrsgmt <hrsgmt@gmail.com>
2026-02-10 18:58:53 +0530

    force crash

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-import Fastify from "fastify";
-
-const app = Fastify();
-
-app.get("/", async () => {
-  return "AXIOM_BACKEND_OK";
-});
-
-await app.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" });
-
-console.log("SERVER_STARTED");
+throw new Error("BOOT_CRASH_TEST")
