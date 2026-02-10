e61c913958b0eeb951293215e547887f9f8f6ec6
hrsgmt <hrsgmt@gmail.com>
2026-02-10 18:57:49 +0530

    nuclear boot test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-import "./src/server.js";
+import Fastify from "fastify";
+
+const app = Fastify();
+
+app.get("/", async () => {
+  return "AXIOM_BACKEND_OK";
+});
+
+await app.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" });
+
+console.log("SERVER_STARTED");
