ba784100617d0c3b46f169c0b61b2f8bce88bc7e
hrsgmt <hrsgmt@gmail.com>
2026-02-10 22:36:15 +0530

    activate real backend

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-console.log("STEP_1_INDEX_START");
-import http from "http";
-
-const server = http.createServer((req, res) => {
-  res.end("AXIOM_BACKEND_ALIVE");
-});
-
-server.listen(process.env.PORT || 3000, "0.0.0.0");
+import "./src/server.js";
